79b8781440d27b6696cc2dbf297d1ea6893dd6fb
Levi Motta <64503782+levizoca@users.noreply.github.com>
2022-10-20 11:35:38 -0300

    Revert "connecting to database and user entity"
    This reverts commit eb361b11c0a6df036f859ced92ba53eecc2c171a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
