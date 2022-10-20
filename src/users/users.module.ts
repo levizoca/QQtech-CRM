eb361b11c0a6df036f859ced92ba53eecc2c171a
Levi Motta <64503782+levizoca@users.noreply.github.com>
2022-10-19 21:55:51 -0300

    connecting to database and user entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService]
-})
+@Module({})
 export class UsersModule {}
