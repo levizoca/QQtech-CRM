61f3271d37e882fa8062cf6c682d541ed6b019f4
Levi Motta <64503782+levizoca@users.noreply.github.com>
2022-10-23 21:43:17 -0300

    added templates and routes mapping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/login')
+  @Render('login')
+  login() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/home')
+  @Render('home')
+  home() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/cadastroColab')
+  @Render('cadastroColab')
+  cadastroColab() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/cadastroCRM')
+  @Render('cadastroCRM')
+  cadastroCRM() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/crmView')
+  @Render('crmView')
+  crmView() {
+    return { message: 'Hello world!' };
+  }
+
+  @Get('/atualizarCRM')
+  @Render('atualizarCRM')
+  atualizarCRM() {
+    return { message: 'Hello world!' };
   }
 }
