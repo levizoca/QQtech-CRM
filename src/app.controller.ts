7d8c6624d10a84fb4b5ed595a1cdabc6a1ba5fee
Levi Motta <64503782+levizoca@users.noreply.github.com>
2022-11-03 21:06:04 -0300

    removed templates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,33 +5,4 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/login')
-  @Render('login')
-  login() {
-    return { message: 'Hello world!' };
-  }
-
-  @Get('/cadastro/colaborador')
-  @Render('cadastroColaborador')
-  cadastroColaborador() {
-    return { message: 'Hello world!' };
-  }
-
-  @Get('/cadastro/crm')
-  @Render('cadastroCRM')
-  cadastroCRM() {
-    return { message: 'Hello world!' };
-  }
-
-  @Get('/visualizar/crm')
-  @Render('visualizarCRM')
-  crmView() {
-    return { message: 'Hello world!' };
-  }
-
-  @Get('/atualizar/crm')
-  @Render('atualizarCRM')
-  atualizarCRM() {
-    return { message: 'Hello world!' };
-  }
 }
